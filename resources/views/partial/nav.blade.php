d7ce786235afd73655542666a981aaf5023ee14b
gauthier dewit <gauthierdewit@gmail.com>
2021-05-06 14:33:22 +0200

    navbar ok

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -23,11 +23,11 @@
 
 
 <!-- ======= Header ======= -->
-<header id="header bg-dark">
+<header id="header">
     <div class="d-flex flex-column">
 
       <div class="profile">
-        <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+        <img src="img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
         <h1 class="text-light"><a href="index.html">Alex Smith</a></h1>
         <div class="social-links mt-3 text-center">
           <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
