71fea9bdcfbd3b17038928e194684ea5d560c21a
gauthier dewit <gauthierdewit@gmail.com>
2021-05-06 16:18:52 +0200

    creation du backoffice

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,3 @@ Route::get('/', function () {
 
 
 
-Route::get('/section1', function () {
-    $page="section1";
-    return view('section1', compact("page"));
-})->name("section1");
-
-Route::get('/section2', function () {
-    $page="section2";
-    return view('section2', compact("page"));
-})->name("section2");
-
-Route::get('/section3', function () {
-    $page="section3";
-    return view('section3', compact("page"));
-})->name("section3");
